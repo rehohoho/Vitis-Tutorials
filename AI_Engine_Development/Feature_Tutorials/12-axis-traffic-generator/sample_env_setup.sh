00069fbe2404c35ed5636438c5e1bc36437c21f1
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-11 15:25:19 -0700

    Feature12/make: package/, sources

---
+++ AI_Engine_Development/Feature_Tutorials/12-axis-traffic-generator/sample_env_setup.sh
@@ -0,0 +1,8 @@
+export PLATFORM_REPO_PATHS=/tools/Xilinx/Vitis/2022.2/base_platforms
+export XILINX_VITIS=/tools/Xilinx/Vitis/2022.2
+export COMMON_IMAGE_VERSAL=/tools/xilinx-versal-common-v2022.2
+
+source $COMMON_IMAGE_VERSAL/environment-setup-cortexa72-cortexa53-xilinx-linux
+source $XILINX_VITIS/settings64.sh
+
+export DSPLIB_ROOT=/tools/Xilinx/Vitis_Libraries/dsp
