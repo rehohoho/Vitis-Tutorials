cc902e3a79b7664b5756ed1dfd376fa4f69ff4ba
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-16 17:12:03 -0700

    Design01: add graph throughput event profile api

--- AI_Engine_Development/Design_Tutorials/01-aie_lenet_tutorial/design/aie_src/graph.cpp
+++ AI_Engine_Development/Design_Tutorials/01-aie_lenet_tutorial/design/aie_src/graph.cpp
@@ -16,17 +16,45 @@ limitations under the License. */
 
 SimGraph simGraph;
 
-#if defined(__AIESIM__) || defined(__X86SIM__)
-int main(int argc, char ** argv) {
-	simGraph.init();
-  
+// for performance calculation
+#define PLIN1_LEN 576 * 32
+#define PLOUT1_LEN 576 * 8
+#define ITER_CNT 1
+
+int get_graph_throughput_by_port(
+  const std::string& log_name,
+  adf::IoAttr& plio_port, 
+  const size_t window_len, 
+  const size_t sample_bytesize) 
+{
+  adf::event::handle edhl = adf::event::start_profiling(plio_port, adf::event::io_stream_start_to_bytes_transferred_cycles, window_len * ITER_CNT * sample_bytesize);
+
   adf::return_code ret;
-  ret = simGraph.run(1);
+  ret = simGraph.run(ITER_CNT);
   if (ret != adf::ok) {
     printf("Run failed\n");
     return ret;
   }
 
+  long long cycles = 0;
+  if (edhl != adf::event::invalid_handle) {
+    cycles = adf::event::read_profiling(edhl);
+    adf::event::stop_profiling(edhl);
+    printf("%s: Cycles %lld Throughput %f samples/s\n", log_name.c_str(), cycles, (double) window_len * ITER_CNT / (cycles * 1e-9)); //samples/second
+  } else {
+    printf("%s: Cycles is ZERO! (note event profile is not supported for x86sim)\n", log_name.c_str());
+  }
+  return 0;
+}
+
+#if defined(__AIESIM__) || defined(__X86SIM__)
+int main(int argc, char ** argv) {
+	simGraph.init();
+  
+  get_graph_throughput_by_port("plin1", simGraph.plin1, PLIN1_LEN, sizeof(int32));
+  get_graph_throughput_by_port("plout1", simGraph.plout1, PLOUT1_LEN, sizeof(int32));
+
+  adf::return_code ret;
   ret = simGraph.end();
   if (ret != adf::ok){
     printf("End failed\n");
