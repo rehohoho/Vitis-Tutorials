36d07f9aa739cae85379c4597d029940f07a66e6
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-22 22:48:32 -0700

    Design11/vmul: add vmul graph

--- AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.cpp
+++ AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.cpp
@@ -16,14 +16,19 @@ limitations under the License. */
 #include "graph_utils.h"
 
 // instance to be compiled and used in host within xclbin
-MmulAieapiGraph mmul_aieapi;
+// MmulAieapiGraph mmul_aieapi;
+VmulIntrinsicGraph vmul_scalar;
 
 
 #ifdef __X86SIM__
 int main(int argc, char ** argv) {
-	adfCheck(mmul_aieapi.init(), "init mmul_aieapi");
-  adfCheck(mmul_aieapi.run(1), "run mmul_aieapi");
-	adfCheck(mmul_aieapi.end(), "end mmul_aieapi");
+	// adfCheck(mmul_aieapi.init(), "init mmul_aieapi");
+  // adfCheck(mmul_aieapi.run(1), "run mmul_aieapi");
+	// adfCheck(mmul_aieapi.end(), "end mmul_aieapi");
+  
+  adfCheck(vmul_scalar.init(), "init vmul_scalar");
+  adfCheck(vmul_scalar.run(1), "run vmul_scalar");
+	adfCheck(vmul_scalar.end(), "end vmul_scalar");
   return 0;
 }
 #endif
@@ -32,11 +37,17 @@ int main(int argc, char ** argv) {
 #ifdef __AIESIM__
 int main(int argc, char ** argv) {
 	
-	adfCheck(mmul_aieapi.init(), "init mmul_aieapi");
-  get_graph_latency(mmul_aieapi, "plin[0]/plout[0]", mmul_aieapi.plin[0], mmul_aieapi.plout[0], ITER_CNT);
-  get_graph_throughput_by_port(mmul_aieapi, "plin[0]", mmul_aieapi.plin[0], A_LEN, sizeof(int8), ITER_CNT);
-  get_graph_throughput_by_port(mmul_aieapi, "plout[0]", mmul_aieapi.plout[0], C_LEN, sizeof(int8), ITER_CNT);
-	adfCheck(mmul_aieapi.end(), "end mmul_aieapi");
+	// adfCheck(mmul_aieapi.init(), "init mmul_aieapi");
+  // get_graph_latency(mmul_aieapi, "plin[0]/plout[0]", mmul_aieapi.plin[0], mmul_aieapi.plout[0], ITER_CNT);
+  // get_graph_throughput_by_port(mmul_aieapi, "plin[0]", mmul_aieapi.plin[0], A_LEN, sizeof(int8), ITER_CNT);
+  // get_graph_throughput_by_port(mmul_aieapi, "plout[0]", mmul_aieapi.plout[0], C_LEN, sizeof(int8), ITER_CNT);
+	// adfCheck(mmul_aieapi.end(), "end mmul_aieapi");
+
+  adfCheck(vmul_scalar.init(), "init vmul_scalar");
+  get_graph_latency(vmul_scalar, "plin[0]/plout[0]", vmul_scalar.plin[0], vmul_scalar.plout[0], ITER_CNT);
+  get_graph_throughput_by_port(vmul_scalar, "plin[0]", vmul_scalar.plin[0], VMUL_A_LEN, sizeof(int16), ITER_CNT);
+  get_graph_throughput_by_port(vmul_scalar, "plout[0]", vmul_scalar.plout[0], VMUL_M, sizeof(int16), ITER_CNT);
+	adfCheck(vmul_scalar.end(), "end vmul_scalar");
   return 0;
 }
 #endif
--- AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.h
+++ AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.h
@@ -3,6 +3,7 @@
 
 #include <adf.h>
 #include "mmul.h"
+#include "mmul_intrinsics.h"
 
 #define ITER_CNT  1
 #define ROW_A_LEN 64
@@ -56,5 +57,46 @@ class MmulAieapiGraph : public adf::graph {
     }
 
 };
+
+
+#define VMUL_M      64
+#define VMUL_N      16
+#define VMUL_A_LEN  VMUL_N * VMUL_M
+
+class VmulIntrinsicGraph : public adf::graph {
+
+  private:
+    adf::kernel k[1];
+
+  public:
+    adf::input_plio plin[2];
+    adf::output_plio plout[1];
+
+    VmulIntrinsicGraph() { 
+      k[0] = adf::kernel::create(vmul_intrinsic_scalar<VMUL_M, VMUL_N>);
+      
+      for (int i = 0; i < 1; i++)
+        adf::source(k[i]) = "mmul_intrinsics.cc";
+
+#ifdef EXTERNAL_IO
+      plin[0] = adf::input_plio::create("vscalar_plin0", adf::plio_64_bits);
+      plin[1] = adf::input_plio::create("vscalar_plin1", adf::plio_64_bits);
+      plout[0] = adf::output_plio::create("vscalar_plout0", adf::plio_64_bits);
+#else
+      plin[0] = adf::input_plio::create("vscalar_plin0", adf::plio_64_bits, "vmula_100samples.txt");
+      plin[1] = adf::input_plio::create("vscalar_plin1", adf::plio_64_bits, "vmulb_100samples.txt");
+      plout[0] = adf::output_plio::create("vscalar_plout0", adf::plio_64_bits, "vmul_scalar.txt");
+#endif
+      
+      adf::connect<adf::window<VMUL_A_LEN*2>> (plin[0].out[0], k[0].in[0]);
+      adf::connect<adf::window<VMUL_N*2>> (plin[1].out[0], k[0].in[1]);
+      adf::connect<adf::window<VMUL_M*2>> (k[0].out[0], plout[0].in[0]);
+      
+      for (int i = 0; i < 1; i++)
+        adf::runtime<ratio>(k[i]) = 0.6;
+    }
+
+};
+
 
 #endif // __GRAPH_H__
