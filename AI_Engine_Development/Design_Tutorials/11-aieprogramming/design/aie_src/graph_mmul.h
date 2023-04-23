30d5adc4e1aa33bfd29a864cb01aa96adb31a4b8
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-22 17:20:48 -0700

    Design11/mmul: update graph and make for shuffle kernels

--- AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.h
+++ AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/graph_mmul.h
@@ -16,30 +16,42 @@
 class MmulAieapiGraph : public adf::graph {
 
   private:
-    adf::kernel k[1];
+    adf::kernel k[4];
 
   public:
     adf::input_plio plin[2];
     adf::output_plio plout[1];
 
     MmulAieapiGraph() { 
-      k[0] = adf::kernel::create_object<MmulAieapi<ROW_A_LEN, COL_A_LEN, COL_B_LEN, 0>>();
-      adf::source(k[0]) = "mmul.cc";
+      k[0] = adf::kernel::create(mmul_aieapi_shuffleA<ROW_A_LEN, COL_A_LEN, COL_B_LEN, 0>);
+      k[1] = adf::kernel::create(mmul_aieapi_shuffleB<ROW_A_LEN, COL_A_LEN, COL_B_LEN, 0>);
+      k[2] = adf::kernel::create_object<MmulAieapi<ROW_A_LEN, COL_A_LEN, COL_B_LEN, 0>>();
+      k[3] = adf::kernel::create(mmul_aieapi_shuffleC<ROW_A_LEN, COL_A_LEN, COL_B_LEN, 0>);
+      
+      for (int i = 0; i < 4; i++)
+        adf::source(k[i]) = "mmul.cc";
 
 #ifdef EXTERNAL_IO
       plin[0] = adf::input_plio::create("mmul_plin0", adf::plio_64_bits);
       plin[1] = adf::input_plio::create("mmul_plin1", adf::plio_64_bits);
       plout[0] = adf::output_plio::create("mmul_plout0", adf::plio_64_bits);
 #else
-      plin[0] = adf::input_plio::create("mmul_plin0", adf::plio_64_bits, "mmul_100samples.txt");
-      plin[1] = adf::input_plio::create("mmul_plin1", adf::plio_64_bits, "mmul_100samples.txt");
+      plin[0] = adf::input_plio::create("mmul_plin0", adf::plio_64_bits, "mmul_100samples_rand.txt");
+      plin[1] = adf::input_plio::create("mmul_plin1", adf::plio_64_bits, "mmul_100samples_rand.txt");
       plout[0] = adf::output_plio::create("mmul_plout0", adf::plio_64_bits, "mmul_aieapi.txt");
 #endif
       
       adf::connect<adf::window<A_LEN>> (plin[0].out[0], k[0].in[0]);
-      adf::connect<adf::window<B_LEN>> (plin[1].out[0], k[0].in[1]);
-      adf::connect<adf::window<C_LEN>> (k[0].out[0], plout[0].in[0]);
-      adf::runtime<ratio>(k[0]) = 0.6;
+      adf::connect<adf::window<B_LEN>> (plin[1].out[0], k[1].in[0]);
+      
+      adf::connect<adf::window<A_LEN>> (k[0].out[0], k[2].in[0]);
+      adf::connect<adf::window<B_LEN>> (k[1].out[0], k[2].in[1]);
+      adf::connect<adf::window<C_LEN>> (k[2].out[0], k[3].in[0]);
+      
+      adf::connect<adf::window<C_LEN>> (k[3].out[0], plout[0].in[0]);
+      
+      for (int i = 0; i < 4; i++)
+        adf::runtime<ratio>(k[i]) = 0.6;
     }
 
 };
