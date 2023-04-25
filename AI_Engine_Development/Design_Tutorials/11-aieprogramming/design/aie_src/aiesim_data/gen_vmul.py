533cf7fdb1ceb82bafcab8c1c665bcb329a82775
rehohoho <ernie.birdy.ho@gmail.com>
2023-04-24 18:31:49 -0700

    Design11/vmul: update data, add golden

--- AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/aiesim_data/gen_vmul.py
+++ AI_Engine_Development/Design_Tutorials/11-aieprogramming/design/aie_src/aiesim_data/gen_vmul.py
@@ -5,19 +5,27 @@ N = 16
 samples = 100
 bitwidth = 64
 datasize = 16
+n_per_row = bitwidth // datasize
 
-# column based
+# column major
 col = np.tile(np.arange(N), samples)
-data = np.repeat(col[:, None], M, axis=1)
+matA_out = np.repeat(col[:, None], M, axis=1)
+matA = matA_out.reshape(samples, N, M).transpose(0, 2, 1)
 np.savetxt("vmula_100samples.txt", 
-           data.reshape(-1, bitwidth//datasize), fmt="%d")
+           matA_out.reshape(-1, n_per_row), fmt="%d")
 np.savetxt("vmula_100samples_phase1.txt", 
-           data[::2,:].reshape(-1, bitwidth//datasize), fmt="%d")
+           matA_out[::2,:].reshape(-1, n_per_row), fmt="%d")
 np.savetxt("vmula_100samples_phase2.txt", 
-           data[1::2,:].reshape(-1, bitwidth//datasize), fmt="%d")
+           matA_out[1::2,:].reshape(-1, n_per_row), fmt="%d")
 
-# row based
-data = np.arange(M)
-data = np.repeat(data[None, :], 100, axis=0)
-data = data.reshape(-1, bitwidth//datasize)
-np.savetxt("vmulb_100samples.txt", data, fmt="%d")
+# column major
+row = np.arange(N)
+matB = np.repeat(row[None, :], samples, axis=0)
+np.savetxt("vmulb_100samples.txt", matB.reshape(-1, n_per_row), fmt="%d")
+
+# golden
+res = []
+for a, b in zip(matA, matB):
+  res.append(a.dot(b))
+res = np.array(res, dtype=np.int16)
+np.savetxt("vmulc_100samples.txt", res.reshape(-1, n_per_row), fmt="%d")
